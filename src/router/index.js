0c3e49cd430b2a83e6784d2a6c3a43443af00ad5
Lincxx <bossco28@gmail.com>
2020-03-26 13:06:10 -0400

    Added in new views login, public, private and did some cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,39 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Public from '../views/Public.vue'
+import Private from '../views/Private.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/Login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/Public',
+    name: 'Public',
+    component: Public
+  },
+  {
+    path: '/Private',
+    name: 'Private',
+    component: Private
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    user: {}
+  },
+  getters: {
+    getUser: state => state.user
+  },
+  mutations: {
+    settingUser: (state, user) => (state.user = user)
+  },
+  actions: {
+    setUset: ({ commit }, user) => commit('settingUser', user)
+  },
+
+})
